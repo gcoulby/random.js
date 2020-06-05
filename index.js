bada8555d33f7eb58a6226041ee7273cdfc8cb05
GrahamCoulby <gcoulby@googlemail.com>
2020-06-05 17:09:01 +0100

    removed overloads, not supported by JS

--- index.js
+++ index.js
@@ -1,36 +1,14 @@
 class Random {
-    next() {
-        let min = 0;
-        let max = Number.MAX_SAFE_INTEGER;
+    next(min = 0, max = Number.MAX_SAFE_INTEGER) {
         return Math.floor(Math.random() * (max - min + 1) + min);
     }
 
-    next(max) {
-        let min = 0;
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    next(min, max) {
-        return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-
-    nextFloat() {
-        let min = 0;
-        let max = parseFloat(Number.MAX_SAFE_INTEGER);
-        return Math.random() * (max - min + 1) + min;
-    }
-
-    nextFloat(max) {
-        let min = 0;
-        return Math.random() * (max - min + 1) + min;
-    }
-
-    nextFloat(min, max) {
+    nextFloat(min = 0, max = Number.MAX_SAFE_INTEGER) {
         return Math.random() * (max - min + 1) + min;
     }
 
     sample() {
-        return Math.Random();
+        return Math.random();
     }
 }
 
